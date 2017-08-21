8abacbcbddeb2b1cb7b82bf2cc3565e4d67d0786
ren1995520@gmail.com <rzh110120999>
2017-08-21 16:43:59 +0800

    怎讲

---
+++ 111/test_count.py
@@ -0,0 +1,33 @@
+import unittest
+from count import  Calculator
+
+class CountTest(unittest.TestCase):#创建CountTest 类继承 unittest.TestCase 类。
+
+    def setUp(self):#初始化
+        self.cal=Calculator(8,4)
+    def tearDown(self):#结束
+        pass
+    def test_add(self):
+        result=self.cal.add()
+        self.assertEqual(result,12)
+    def test_sub(self):
+        result=self.cal.sub()
+        self.assertEqual(result,4)
+    def test_mul(self):
+        result =self.cal.mul()
+        self.assertEqual(result,32)
+    def test_div(self):
+        result=self.cal.div()
+        self.assertEqual(result,2)
+
+if __name__ == "__main__":
+    # unittest.main()
+    #  构造测试集
+    suite=unittest.TestSuite()
+    suite.addTest(CountTest("test_add"))
+    suite.addTest(CountTest("test_sub"))
+    suite.addTest(CountTest("test_mul"))
+    suite.addTest(CountTest("test_div"))
+    # 执行测试
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
