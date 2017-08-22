04ca3abc00159def650d86e2a34e2cabd52e66b9
ren1995520@gmail.com <rzh110120999>
2017-08-22 18:39:41 +0800

    添加接口

--- sign/views_if.py
+++ sign/views_if.py
@@ -3,13 +3,14 @@ from sign.models import Event
 from django.core.exceptions import ValidationError
 #添加发布会接口
 def add_event(request):
+    #通过post请求接受发布会的参数
     eid=request.POST.get('eid','')#发布会id
     name=request.POST.get('name','')#名称
     limit=request.POST.get('limit','')#限制人数
     status=request.POST.get('status','')#状态
     address=request.post.get('address','')#地址
     start_time=request.POST.get('start_time','')#发布会时间
-
+    #首先判断不能为空
     if eid==''or name==''or limit==''or address=='' or start_time=='':
         return JsonResponse({'status':10021,'message':'parameter error'})
     result=Event.objects.filter(id=eid)
