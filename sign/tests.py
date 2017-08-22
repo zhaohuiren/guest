ec951993a673c7c7af1c65c9a8b4eb964b7490bc
ren1995520@gmail.com <rzh110120999>
2017-08-22 14:56:09 +0800

    测试框架

--- sign/tests.py
+++ sign/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from sign.models import Event,Guest
+from django.test import Client
+from django.contrib.auth.models import User
+from datetime import  datetime
 # Create your tests here.
 class ModelTest(TestCase):
     def setUp(self):
@@ -30,3 +33,35 @@ class ModelTest(TestCase):
         result = Guest.objects.get(phone='13121641251')
         self.assertEqual(result.realname, "alen")
         self.assertFalse(result.sign)
+class IndexPageTest(TestCase):
+    '''测试index登录首页 '''
+    def test_index_page_renders_index_template(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'index.html')
+
+
+class LoginActionTesst(TestCase):
+    '''测试登录函数'''
+    def setUp(self):
+        User.objects.create_user('admin','admin@mail.com','admin123456')
+        self.c=Client()
+
+    def test_login_action_username_password_null(self):
+        '''用户名密码为空'''
+        test_data={'username':'','password':''}
+        respones=self.c.post('/login_action/',data=test_data)
+        self.assertEqual(respones.status_code,200)
+        self.assertIn(b"error",respones.content)
+    def test_login_action_uesname_password_error(self):
+        '''用户名密码错误'''
+        test_date={'username':'abc','password':'123'}
+        respones=self.c.post('/login_action/',data=test_date)
+        self.assertEqual(respones.status_code,200)
+        self.assertIn(b"error",respones.content)
+    def test_login_action_successs(self):
+        '''登录成功'''
+        test_data={'username':'admin','password':'rzh110120999'}
+        response=self.c.post('/login_action/',data=test_data)
+        self.assertEqual(response.status_code,302)
+
